fa5efb91de24c015d9205f420543592b669e5e18
michael marsden <147929276+michaelm2006@users.noreply.github.com>
2026-08-29 20:59:38 +0100

    Working state

--- src/main/java/wtf/sable/mixin/MinecraftMixin.java
+++ src/main/java/wtf/sable/mixin/MinecraftMixin.java
@@ -9,8 +9,29 @@ import wtf.sable.FarmingMacro;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+    private static boolean forward = true;
+    private static boolean wasMoving = false;
+
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
+        if (FarmingMacro.ENABLED) {
+            Minecraft mc = Minecraft.getInstance();
 
+            boolean moving = mc.player.getDeltaMovement().horizontalDistanceSqr() > 0.0001;
+            boolean vertMove = mc.player.getDeltaMovement().y > 0.0001;
+
+            if (!vertMove) {
+                if (wasMoving && !moving) {
+                    forward = !forward;
+                }
+
+                wasMoving = moving;
+            }
+
+            mc.options.keyUp.setDown(forward);
+            mc.options.keyDown.setDown(!forward);
+
+            mc.options.keyAttack.setDown(true);
+        }
     }
 }
